deed137c270993d2a20d4f623ccd9864d685b79f
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2020-03-10 11:35:46 +0200

    Class activity on Data Types 2

---
+++ DataTypeExercise2.py
@@ -0,0 +1,32 @@
+#declare list
+cars = [56,78,34,21,56,125,45,89,75,12,56]
+
+#find the smallest number
+smallNumber = min(cars)
+
+#find the largest number
+largeNumber = max(cars) 
+
+#print smallest number 
+print(smallNumber)
+
+#print largest number
+print(largeNumber)
+
+#find the sum of the two numbers
+sum = smallNumber + largeNumber
+
+#print the sum of the two numbers
+print(sum)
+
+#find none dupliactes and append to a new list
+newCars = []
+for i in cars:
+    if i not in newCars:
+        newCars.append(i)
+    
+#make old cars be equal to new cars
+cars = newCars
+
+#print the corrected list
+print(cars)
