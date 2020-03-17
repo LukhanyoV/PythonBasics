b3a6cadbd4fbfedcb3af2a5f26cd8687c1268a91
Lukhanyo Vakele <47126706+LukhanyoV@users.noreply.github.com>
2020-03-17 15:47:03 +0200

    Removed the function to make it simpler

--- numberGuess.py
+++ numberGuess.py
@@ -1,19 +1,17 @@
 from random import randrange
 
-def playGame():
-    mysteryNumber = randrange(1, 10, 1)
-    guesses_left = 3
-    print("I have a number thaat ranges from 0-10\nWhat's my number?")
-    while guesses_left != 0:
-        guess = int(input("Enter your guess: "))
-        if guess == mysteryNumber:
-            print("Yay! You won\nThe number was ",str(mysteryNumber))
-            return True
-        else:
-            guesses_left -= 1
-            print("Opps sorry :(\nGuesses remaning:",str(guesses_left),"\n")
+mysteryNumber = randrange(1, 10, 1)
+guesses_left = 3
+print("I have a number thaat ranges from 0-10\nWhat's my number?")
+while True:
+    guess = int(input("Enter your guess: "))
+    if guess == mysteryNumber:
+        print("Yay! You won\nThe number was ",str(mysteryNumber))
+        break
+    else:
+        guesses_left -= 1
+        print("Opps sorry :(\nGuesses remaning:",str(guesses_left),"\n")
 
-
-ans = playGame()
-if ans != True:
-    print("Better Luck Next Time!")
+    if guesses_left == 0:
+        print("Better Luck Next Time!"+"\nThe number was: "+str(mysteryNumber))
+        break
